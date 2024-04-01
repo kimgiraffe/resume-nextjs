3f900a9f16d499ad2364f169b58c33a27d6d39de
kimgiraffe <kimgiraffe@naver.com>
2024-04-01 21:37:28 +0900

    feat: add introduce message

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -3,7 +3,11 @@ import { IIntroduce } from '../component/introduce/IIntroduce';
 const introduce: IIntroduce.Payload = {
   disable: false,
 
-  contents: ['글로벌 역량을 갖춘 FE 개발자 김세민입니다.'],
+  contents: [
+    '안녕하세요! 성장하는 개발자 김세민입니다.',
+    '사용자 인터페이스/경험에 관심이 많습니다.',
+    '최신기술에 관심이 많고, 프로젝트에 적용해보며 내 것으로 만드려고 노력합니다. ',
+  ],
   sign: 'semin',
   // sign: packageJson.author.name,
   // latestUpdated: '2019-07-24',
