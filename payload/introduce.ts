4acd72db53af63e8a85f9c7ac3c48ae9aa69a322
kimgiraffe <kimgiraffe@naver.com>
2024-05-24 10:05:14 +0900

    feat : update introduce

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -6,12 +6,12 @@ const introduce: IIntroduce.Payload = {
   contents: [
     '안녕하세요! 성장하는 개발자 김세민입니다.',
     '사용자 인터페이스/경험에 관심이 많습니다.',
-    '최신기술에 관심이 많고, 프로젝트에 적용해보며 내 것으로 만드려고 노력합니다.',
+    '최신기술에 관심이 많고, 프로젝트에 적용해보며 내 것으로 만들려고 노력합니다.',
   ],
   sign: 'semin',
   // sign: packageJson.author.name,
   // latestUpdated: '2019-07-24',
-  latestUpdated: '2024-04-03',
+  latestUpdated: '2024-05-24',
 };
 
 export default introduce;
